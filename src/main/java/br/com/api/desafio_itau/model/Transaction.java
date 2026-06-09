ce38eedd9c3980ef80d516fbd7e148ae2478d9ae
F4314N0-JUN10R <f4bianojunior091101@gmail.com>
2026-06-09 18:27:39 -0300

    refactor: aimplementação da model/objeto principal com as validações de dados

--- src/main/java/br/com/api/desafio_itau/model/Transaction.java
+++ src/main/java/br/com/api/desafio_itau/model/Transaction.java
@@ -2,17 +2,26 @@ package br.com.api.desafio_itau.model;
 
 
 import java.math.BigDecimal;
-
 import java.time.OffsetDateTime;
 
-public record Transaction(BigDecimal value, OffsetDateTime dateTime) {
-    public Transaction {
-        if (value == null || value.compareTo(BigDecimal.ZERO) < 0) {
-            throw new RuntimeException("A transação não pode ser negativa!");
+public class Transaction {
+    private final BigDecimal value;
+    private final OffsetDateTime dateTime;
+
+    public Transaction(TransactionDTO dados) {
+        if (dados.dateTime() != null && dados.dateTime().isAfter(OffsetDateTime.now())) {
+            throw new IllegalArgumentException("A transação não pode ser realizada no futuro");
+        }
+        if (dados.value() == null ||  dados.value().compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("A Transação não pode ser nula ou menor que zero");
         }
 
-        if (dateTime != null && dateTime.isAfter(OffsetDateTime.now())) {
-            throw new RuntimeException("A transação não pode ser realizada no futuro");
-        }
+        this.value = dados.value();
+        this.dateTime = dados.dateTime();
     }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
 }
