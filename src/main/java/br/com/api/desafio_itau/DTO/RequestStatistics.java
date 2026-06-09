b20e06147b7434c9e18bfa0ed1c743eacf1b48a8
F4314N0-JUN10R <f4bianojunior091101@gmail.com>
2026-06-09 18:25:55 -0300

    refactor: centralizar lógica de cálculo de estatísticas

--- src/main/java/br/com/api/desafio_itau/DTO/RequestStatistics.java
+++ src/main/java/br/com/api/desafio_itau/DTO/RequestStatistics.java
@@ -1,4 +1,4 @@
-package br.com.api.desafio_itau.statistics;
+package br.com.api.desafio_itau.DTO;
 
 import br.com.api.desafio_itau.model.Transaction;
 
@@ -6,28 +6,28 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
 
-public record Statistics(
-        int count,
+public record RequestStatistics(
+        long count,
         BigDecimal sum,
         BigDecimal avg,
         BigDecimal min,
         BigDecimal max) {
 
-    public static Statistics calculate(List<Transaction> transactions){
+    public static RequestStatistics calculate(List<Transaction> transactions){
         //tratamento para lista vazia evita divisão por zero
         if (transactions == null || transactions.isEmpty()){
-            return new Statistics(0, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO);
+            return new RequestStatistics(0, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO, BigDecimal.ZERO);
         }
         //inicia as variaveis pra não ficarem nulas
         int count = 0;
         BigDecimal sum = BigDecimal.ZERO;
 
         // pega a primeira transação da lista
-        BigDecimal min = transactions.get(0).value();
-        BigDecimal max = transactions.get(0).value();
+        BigDecimal min = transactions.get(0).getValue();
+        BigDecimal max = transactions.get(0).getValue();
 
         for (Transaction t : transactions) {
-            BigDecimal currentValue = t.value();
+            BigDecimal currentValue = t.getValue();
 
             count++;
 
@@ -43,11 +43,11 @@ public record Statistics(
             }
         }
 
-        // calcula a media usando o contador como divisor e dividendo
+        // calcula a média usando o contador como divisor e dividendo
         BigDecimal divider = BigDecimal.valueOf(count);
         BigDecimal avg = sum.divide(divider, 2, RoundingMode.HALF_UP);
 
 
-        return new Statistics(count, sum, avg, min, max);
+        return new RequestStatistics(count, sum, avg, min, max);
     }
 }
